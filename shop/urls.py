e30559efff3bd86a843fd9dc55dce26104d8e957
Priyanshu Raj <rpriyanshu9@gmail.com>
2020-07-24 12:07:36 +0530

    third commit

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index , name="ShopHome"),
-    path("about/", views.about , name="AboutUs"),
-    path("contact/", views.contact , name="ContactUs"),
-    path("tracker/", views.tracker , name="TrackingStatus"),
-    path("products/<int:myid>", views.productView , name="ProductView"),
-    path("search", views.search , name="Search"),
-    path("checkout/", views.checkout , name="Checkout"),
-    path("handlerequest/", views.handlerequest , name="handlerequest"),
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("search/", views.search, name="Search"),
+    path("checkout/", views.checkout, name="Checkout"),
+    path("handlerequest/", views.handlerequest, name="handlerequest"),
 ]
